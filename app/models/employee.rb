8c11296994ad4be7ec675c3ebd1701863cbdc79c
sulavr <cyberraid@gmail.com>
2018-05-04 19:11:00 +0545

    ror

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,25 @@
-class Employee < ApplicationRecord
-def update_phone_number
+class Employee<ApplicationRecord
+  def update_phone_number ph_number
+    self.update(phone_number=ph_number)
+  end
+  def self.sorting_employees
+    order(first_name: :asc)
 
-add
-end
-end
+  end
+  def display_full_name
+    "#{first_name} #{middle_name} #{last_name}"
+  end
+  def self.calculate_salary
+    average_salary=average(:salary).to_f
+    minimum_salary=minimum(:salary)
+    maximim_salary=maximum(:salary)
+    salary_array=[]
+    salary_array.push average_salary
+    salary_array.push minimum_salary
+    salary_array.push maximim_salary
+  end
+  def self.delete_employees
+    delete_all
+
+  end
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_04_085036) do
+ActiveRecord::Schema.define(version: 2018_05_04_130943) do
 
   create_table "employees", force: :cascade do |t|
     t.string "first_name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_05_04_085036) do
     t.integer "salary"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "designation"
   end
 
 end
