af45c51aa3f97caa638e85ccea05e424741fb249
sulavr <cyberraid@gmail.com>
2018-05-06 18:31:22 +0545

    model_validation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,12 @@
 class Employee<ApplicationRecord
-  validates :first_name ,:last_name ,presence: true
+  validates :first_name ,:last_name ,presence: {message: "Please provide your first and last name"}
+  validates :phone_number, length: {is: 10}, format: { with: /[9]\d{9}/, message:"Number must start with 9"}
+  validates :salary, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :email, format:{with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i},uniqueness: true
+  before_save :ensure_case_matches
+  before_destroy :ask_before_delete
+
+
 
   def update_phone_number ph_number
     self.update(phone_number=ph_number)
@@ -14,14 +21,33 @@ class Employee<ApplicationRecord
   def self.calculate_salary
     average_salary=average(:salary).to_f
     minimum_salary=minimum(:salary)
-    maximim_salary=maximum(:salary)
+    maximum_salary=maximum(:salary)
     salary_array=[]
     salary_array.push average_salary
     salary_array.push minimum_salary
-    salary_array.push maximim_salary
+    salary_array.push maximum_salary
   end
-  def self.delete_employees
+  def self.delete_all_records
     delete_all
 
   end
+
+  def ensure_case_matches
+    self.first_name=first_name.capitalize
+    self.middle_name=middle_name.capitalize
+    self.last_name=last_name.capitalize
   end
+
+  def ask_before_delete
+  puts 'Do you want to delete the record(Y/N)?'
+        answer=gets
+
+    if answer=="y"||"Y"
+      destroy
+    else
+    puts "Record is not deleted"
+  end
+
+  end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_04_130943) do
+ActiveRecord::Schema.define(version: 2018_05_06_095159) do
 
   create_table "employees", force: :cascade do |t|
     t.string "first_name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_05_04_130943) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "designation"
+    t.string "email"
   end
 
 end
